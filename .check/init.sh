69307e4c0d84f0dd489076d1eb1768d21f2462f0
GrzegorzTreter <107022065+g7eg@users.noreply.github.com>
2024-02-15 16:06:15 +0000

    Change paths

--- .check/init.sh
+++ .check/init.sh
@@ -2,7 +2,7 @@
 
 function check_z1(){
 testnum=1
-pathz1=/workspaces/Wprowadzenie_do_CLI/.task_tests/zad1_test.py
+pathz1=./.task_tests/zad1_test.py
 
 if [ -f $pathz1 ]; then
     python3 $pathz1
@@ -14,14 +14,14 @@ fi
 
 function check_z2(){
 
-python3 /workspaces/Wprowadzenie_do_CLI/.task_tests/zad2_test.py
+python3 ./.task_tests/zad2_test.py
 
 }
 
 function check_z3(){
-if [ -f /workspaces/Wprowadzenie_do_CLI/.task_tests/zad3_test.py ]; then
+if [ -f ./.task_tests/zad3_test.py ]; then
     echo 'File exists.'
-    python3 /workspaces/Wprowadzenie_do_CLI/.task_tests/zad3_test.py
+    python3 ./.task_tests/zad3_test.py
 
 else
     echo 'File does not exist.'
